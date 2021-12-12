071cd50e1fd43006b6a1eb61e46607a08f60ac04
hazartilirot <hazartilirot@gmail.com>
2021-12-12 14:01:05 +0200

    Add a new title

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <h1>Hi people</h1>
+    <h2>This is a new title</h2>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <StaticImage
